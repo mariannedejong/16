912ca6c34d416b3ba379d71490fd2b24c800d1db
mariannedejong <89845904+mariannedejong@users.noreply.github.com>
2021-11-09 23:40:17 +0100

    Add files via upload

---
+++ function_basics.js
@@ -0,0 +1,17 @@
+const pizzaMaken = function (){
+    console.log("Maak de bodem");
+    console.log("Doe de ingrediënten op de bodem")
+    console.log("Tien minuten in een voorverwarmde oven (200 graden)")
+};
+
+const sushiMaken = function (){
+    console.log("Kook de rijst");
+    console.log("Snijd de ingrediënten")
+    console.log("Rol de sushi")
+};
+
+pizzaMaken();
+pizzaMaken();
+pizzaMaken();
+sushiMaken();
+sushiMaken();
